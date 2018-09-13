e9f2497439422bb678e447b891864871cc3964ac
Tan Shuai <7anshuai@gmail.com>
2018-09-13 10:37:29 +0800

    Initial commit

---
+++ src/client/index.js
@@ -0,0 +1,42 @@
+import 'babel-polyfill'
+
+import Immutable from 'immutable'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
+import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { isProd } from '../shared/util'
+
+const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+
+const store = createStore(combineReducers({hello: helloReducer }),
+  {hello: Immutable.fromJS(preloadedState.hello)},
+  composeEnhancers(applyMiddleware(thunkMiddleware)))
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrapApp = (AppComponent, reduxStore) =>
+  <Provider store={reduxStore}>
+    <BrowserRouter>
+      <AppContainer>
+        <AppComponent />
+      </AppContainer>
+    </BrowserRouter>
+  </Provider>
+
+ReactDOM.render(wrapApp(App, store), rootEl)
+
+if (module.hot) {
+  module.hot.accept('../shared/app', () => {
+    const NextApp = require('../shared/app').default
+    ReactDOM.render(wrapApp(NextApp, store), rootEl)
+  })
+}
---
+++ src/server/index.js
@@ -0,0 +1,65 @@
+// @flow
+
+import compression from 'compression'
+import express from 'express'
+
+import { STATIC_PATH, WEB_PORT } from '../shared/config'
+import { isProd } from '../shared/util'
+import {
+  HOME_PAGE_ROUTE,
+  HELLO_PAGE_ROUTE,
+  HELLO_ASYNC_PAGE_ROUTE,
+  helloEndpointRoute
+} from '../shared/routes'
+import renderApp from './render-app'
+
+const app = express()
+
+app.use(compression())
+app.use(STATIC_PATH, express.static('dist'))
+app.use(STATIC_PATH, express.static('public'))
+
+app.get(HOME_PAGE_ROUTE, (req, res) => {
+  res.send(renderApp(req.url))
+})
+
+app.get(HELLO_PAGE_ROUTE, (req, res) => {
+  res.send(renderApp(req.url, () => ({
+    hello: {
+      message: 'Server-side preloaded message'
+    }
+  })))
+})
+
+app.get(HELLO_ASYNC_PAGE_ROUTE, (req, res) => {
+  res.send(renderApp(req.url, () => ({
+    hello: {
+      messageAsync: 'Server-side preloaded message for async page'
+    }
+  })))
+})
+
+app.get(helloEndpointRoute(), (req, res) => {
+  res.json({ serverMessage: `Hello from the server! (received ${req.params.num})` })
+})
+
+app.get('/500', () => {
+  throw Error('Internal Server Error')
+})
+
+app.get('*', (req, res) => {
+  res.status(404).send(renderApp(req.url))
+})
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.error(err.stack)
+  res.status(500).send('Something went wrong')
+})
+
+app.listen(WEB_PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
+  '(development).\nKeep "yarn dev:client" running in an other terminal'}.`)
+})
